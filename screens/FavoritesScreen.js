a39984b8a46dfb41ed03a4431978ed467fa33eee
Ibnu Naufal <ibnunaufal5758@gmail.com>
2022-09-11 13:41:33 +0700

    use context & show fav meal

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,38 @@
-import { Text } from 'react-native';
+import { useContext } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MealsList from '../components/MealsList/MealsList';
+import { MEALS } from '../data/dummy-data';
+import { FavoritesContext } from './../store/context/favorites-context'
 
 function FavoritesScreen() {
-  return <Text>The favorites screen!</Text>;
+  const favContext = useContext(FavoritesContext)
+
+  const favMeals = MEALS.filter(meal => favContext.ids.includes(meal.id))
+
+  if(favMeals.length === 0){
+    return (
+      <View style ={styles.rootConteiner}>
+        <Text style={styles.text}>
+          Favourites is Empty !!
+        </Text>
+      </View>
+    )
+  }
+  
+  return <MealsList items={favMeals} />
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootConteiner: {
+    flex:1 ,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 18,
+    fontStyle: 'bold',
+    color: 'white'
+  }
+})
